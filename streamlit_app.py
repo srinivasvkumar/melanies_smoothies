a3d04d0b21a5a232300b0fca45490fc037064a01
srinivasvkumar <96481794+srinivasvkumar@users.noreply.github.com>
2026-06-25 21:47:09 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,4 @@ if time_to_insert:
   
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
