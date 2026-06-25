8c7c4f8431a4f905e402b02092c55baa4f3c4b11
srinivasvkumar <96481794+srinivasvkumar@users.noreply.github.com>
2026-06-25 21:07:06 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -53,4 +52,6 @@ time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
+    cnx=st.connection("snowflake")
+    session=cnx.session()
     st.success('Your Smoothie is ordered!', icon="✅")
